485cbbad044ea1d64fc1b46fdd29af8eab7d787a
Gab <sitehub.cloud@gmail.com>
2022-05-15 22:36:04 +0200

    Add password generator function - not style

--- src/main/java/main/application/controller/NuovaPasswordController.java
+++ src/main/java/main/application/controller/NuovaPasswordController.java
@@ -1,19 +1,41 @@
 package main.application.controller;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import main.application.model.Credenziali;
 
-public class NuovaPasswordController {
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class NuovaPasswordController implements Initializable {
     // dichiarazione delle variabili di scena
     @FXML TextField textFieldUrlSito;
     @FXML TextField textFieldNomeUtente;
     @FXML PasswordField passwordFieldUno;  // indica i due campi in cui viene inserita la password con conferma password
     @FXML PasswordField passwordFieldDue;
     @FXML Button bottoneConfermaInserimento;
+    @FXML Button generaPassword;
+    @FXML Slider lunghezzaPassword;
+    @FXML CheckBox lettereNumeriCheckbox;
+    @FXML CheckBox caratteriSpecialiCheckbox;
+    @FXML CheckBox maiuscoleCheckbox;
+
+    // initialize per aggiungere listener allo slider
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        passwordLength = (int) lunghezzaPassword.getValue();
+        lunghezzaPassword.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                passwordLength = (int) lunghezzaPassword.getValue();
+            }
+        });
+
+    }
 
     // metodo per confermare l'aggiunta della nuova password
     public void confermaNuovaPassword(){
@@ -94,4 +116,80 @@ public class NuovaPasswordController {
 
         return true;
     }
+
+    //metodo per generare password
+    public String generatePassword(){
+        String[] array = new String[passwordLength];
+        if(!lettereNumeriCheckbox.isSelected()){
+            return "Lettere e numeri devono essere selezionati";
+        }else if(!caratteriSpecialiCheckbox.isSelected() && !maiuscoleCheckbox.isSelected()){
+
+            for(int i=0; i<array.length; i++){
+                if(randomNumber(1,0)==1){
+                    array[i] = alphabet[randomNumber(25,0)];
+                }else {
+                    array[i] = ""+randomNumber(9, 0);
+                }
+            }
+        }else if(caratteriSpecialiCheckbox.isSelected() && !maiuscoleCheckbox.isSelected()){
+
+            for(int i=0; i<array.length; i++){
+                int varType = randomNumber(900,0);
+                if(varType>=0 && varType<300){
+                    array[i] = alphabet[randomNumber(25,0)];
+                }else if(varType>=300 && varType<600){
+                    array[i] = ""+randomNumber(9, 0);
+                }else{
+                    array[i] = symbols[randomNumber(symbols.length-1, 0)];
+                }
+            }
+        }else if (!caratteriSpecialiCheckbox.isSelected() && maiuscoleCheckbox.isSelected()){
+
+            for(int i=0; i<array.length; i++){
+                int varType = randomNumber(900,0);
+                if(varType>=0 && varType<300){
+                    array[i] = alphabet[randomNumber(25,0)];
+                }else if(varType>=300 && varType<600){
+                    array[i] = ""+randomNumber(9, 0);
+                }else{
+                    array[i] = alphabet[randomNumber(25,0)].toUpperCase(Locale.ROOT);
+                }
+            }
+        }else if(caratteriSpecialiCheckbox.isSelected() && maiuscoleCheckbox.isSelected()){
+
+            for(int i=0; i<array.length; i++){
+                int varType = randomNumber(1200,0);
+                if(varType>=0 && varType<300){
+                    array[i] = alphabet[randomNumber(25,0)];
+                }else if(varType>=300 && varType<600){
+                    array[i] = ""+randomNumber(9, 0);
+                }else if (varType>=600 && varType<900){
+                    array[i] = alphabet[randomNumber(25,0)].toUpperCase(Locale.ROOT);
+                }else{
+                   array[i] = symbols[randomNumber(symbols.length-1, 0)];
+                }
+            }
+        }
+
+        System.out.println("Password generata: "+arrayConvert(array));
+
+        return "";
+    }
+
+    //metodi e variabili ausiliari per generatePassword()
+    private final String[] alphabet = new String[] {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+    private final String[] symbols = new String[] {"!","#","$","%","&","'","(",")","*","+",",","\"","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"};
+    private int randomNumber(int max, int min){
+        int range = max - min + 1;
+        return  (int)(Math.random() * range) + min;
+    }
+    private int passwordLength;
+    private String arrayConvert(String[] array){
+        StringBuilder finalPsw = new StringBuilder();
+        for (String character: array) {
+            finalPsw.append(character);
+        }
+        return finalPsw.toString();
+    }
+
 }
