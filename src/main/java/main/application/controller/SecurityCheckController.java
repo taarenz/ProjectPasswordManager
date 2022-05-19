8b2d8b6cb7e4fe8b6e4af9b06114d93cc86bf4e6
Gab <sitehub.cloud@gmail.com>
2022-05-19 18:06:48 +0200

    Security check for input password

---
+++ src/main/java/main/application/controller/SecurityCheckController.java
@@ -0,0 +1,62 @@
+package main.application.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import main.application.database.DBHandler;
+import main.application.model.Credenziali;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class SecurityCheckController implements Initializable {
+    // dichiarazione degli oggetti di scena
+    @FXML Parent root;
+    @FXML TextField passwordToCheck;
+    @FXML AnchorPane resultPane;
+    @FXML Label resultSummary;
+    @FXML Label resultText;
+    @FXML Button check;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+
+    // password check
+    public void checkPassword(){
+        if(passwordToCheck.getText().length()==0){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Errore");
+            alert.setHeaderText("Campi non compilati");
+            alert.showAndWait();
+        }else{
+            int result = Credenziali.singlePasswordCheck(passwordToCheck.getText());
+            if (result==0){
+                resultPane.setStyle("-fx-background-color:" + "brown");
+                resultSummary.setText("ALTAMENTE CRITICA");
+                resultText.setText("Cambia immediatamente questa password. Ci vogliono pochi secondi per trovarla");
+            }else if (result<10){
+                resultPane.setStyle("-fx-background-color:" + "red");
+                resultSummary.setText("CRITICA");
+                resultText.setText("Cambia questa password: presenta molte vulnerabilità");
+            }else if (result<18){
+                resultPane.setStyle("-fx-background-color:" + "rgba(255,184,54,0.91)");
+                resultSummary.setText("POTENZIALMENTE A RISCHIO");
+                resultText.setText("Questa password presenta delle vulnerabilità, ma potrebbe volerci molto per trovarla");
+            }else{
+                resultPane.setStyle("-fx-background-color:" + "green");
+                resultSummary.setText("OTTIMA");
+                resultText.setText("Questa password è molto sicura. Servono millenni per trovarla ;)");
+            }
+        }
+    }
+}
--- src/main/java/main/application/model/Credenziali.java
+++ src/main/java/main/application/model/Credenziali.java
@@ -1,6 +1,10 @@
 package main.application.model;
 
+import main.application.controller.MainAppController;
+
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Credenziali{
     // attributi
@@ -8,7 +12,6 @@ public class Credenziali{
     private String password;
     private String urlSitoWeb;
     private boolean hasNomeUtente;
-
     private int user_id;
 
     private int id;
@@ -71,5 +74,84 @@ public class Credenziali{
             return "Sito: " + this.urlSitoWeb+ " - User: "+this.nomeUtente+" - Password: "+password;
         else
             return "Sito: " + this.urlSitoWeb+" - Password: "+password;
+    }
+
+    //Password security check
+    public static int singlePasswordCheck(String toCheck){
+        int tempScore = toCheck.length();
+
+        Pattern pattern;
+        Matcher matcher;
+
+        boolean allNumbers = false;
+        for (int i = 0; i < toCheck.length(); i++) {
+            if (Character.isDigit(toCheck.charAt(i))) {
+                allNumbers = true;
+            }
+            else {
+                allNumbers = false;
+                break;
+            }
+        }
+
+        if(allNumbers){
+            return 0;
+        }
+
+        pattern = Pattern.compile("[" + "-/@#!*$%^&.'_+={}()"+ "]+");
+        matcher = pattern.matcher(toCheck);
+        if(matcher.find()){
+            return tempScore++;
+        }
+
+        pattern = Pattern.compile("[A-Z]");
+        matcher = pattern.matcher(toCheck);
+        if(matcher.find()){
+            tempScore+=2;
+        }
+
+        pattern = Pattern.compile("[0-9]");
+        matcher = pattern.matcher(toCheck);
+        if(matcher.find()){
+            tempScore+=3;
+        }
+
+        pattern = Pattern.compile("[^a-zA-Z0-9]");
+        matcher = pattern.matcher(toCheck);
+        if(matcher.find()){
+            tempScore+=4;
+        }
+
+        boolean repeated = false;
+
+        for (int i = 0; i < toCheck.length(); i++) {
+
+            for (int j = 0; j < toCheck.length(); j++) {
+
+                if (toCheck.charAt(i) == toCheck.charAt(j)) {
+                    repeated = true;
+                }
+            }
+        }
+
+        if(repeated){
+            tempScore-=2;
+        }
+
+//        boolean samePassword = false;
+//
+//        for (Credenziali credenziale:
+//             MainAppController.listaCredenzialiUtente) {
+//            if (credenziale.getPassword().equals(toCheck)) {
+//                samePassword = true;
+//                break;
+//            }
+//        }
+//
+//        if (samePassword){
+//            tempScore-=5;
+//        }
+
+        return tempScore;
     }
 }
